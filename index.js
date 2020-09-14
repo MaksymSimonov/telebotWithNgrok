10eb10aa856c672c355f530688a785ae0d9ffc68
MaksymSimonov <simonovmax90@gmail.com>
2020-09-14 20:50:37 +0300

    fixed delay

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 
+const sendNews = require('./utils/sendNews')
 const botRouter = require('./routes/botRouter')
 const credentials = require('./credentials.json')
 const port = credentials.ngrok_port
@@ -9,8 +10,7 @@ const port = credentials.ngrok_port
 app.use(bodyParser.json())
 app.use(botRouter)
 
-let delay = 20000
-const sendNews = require('./utils/sendNews')
+let delay = 300000
 sendNews(delay)
 
 app.listen(port, () => console.log(`Server running on port ${port}`))
